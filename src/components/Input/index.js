8d660a35fb3a90c4392e842df1411cf77ab0bcdb
nikitafloy <nikita5051@yandex.ru>
2022-05-27 16:18:11 +0300

    components(Input): add

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,6 +6,31 @@
  *
  * */
 
-export const Input = () => {
-  return <div className="input">...</div>;
+import "./index.css";
+
+export const Input = ({ value, type, Icon }) => {
+  const inputBoxClasses = ["input-box"];
+  const inputBoxCurrentClasses = ["input-box__current"];
+  if (type) {
+    if (type === "dark") {
+      inputBoxClasses.push("input-box-dark");
+      inputBoxCurrentClasses.push("input-box__current-dark");
+    }
+    if (type === "green") {
+      inputBoxClasses.push("input-box-green");
+      inputBoxCurrentClasses.push("input-box__current-dark");
+      inputBoxCurrentClasses.push("input-box__current-bold");
+    }
+  }
+
+  return (
+    <div className={inputBoxClasses.join(" ")}>
+      {Icon && <img src={Icon} className="input-box__icon" alt="Input Icon" />}
+
+      <input
+        className={inputBoxCurrentClasses.join(" ")}
+        defaultValue={value}
+      />
+    </div>
+  );
 };
