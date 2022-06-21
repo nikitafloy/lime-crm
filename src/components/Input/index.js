934b107497cc4e550e4c964ad3779cc1554a3198
nikitafloy <nikita5051@yandex.ru>
2022-06-21 13:31:33 +0300

    components(Input): refactor

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,10 @@
 import { useEffect, useReducer, useRef } from "react";
 import "./index.scss";
 
+// Imask
 import { useIMask } from "react-imask";
 
+// Helpers
 import { getMask } from "./helpers";
 
 const init = () => {
@@ -11,79 +13,81 @@ const init = () => {
 
 const reducer = (state, action) => getMask(action.type);
 
-export const Input = (props) => {
-  const {
-    defaultValue,
-    style = {},
-    maskType,
-    type,
-    label,
-    theme,
-    Icon,
-    className,
-    onChange,
-    onClick,
-    autoSize,
-    onBlur,
-  } = props;
-
+export const Input = ({
+  defaultValue,
+  style = {},
+  maskType,
+  type,
+  label,
+  theme,
+  Icon,
+  className,
+  onChange,
+  onClick,
+  autoSize,
+  onBlur,
+}) => {
+  const customDateInputRef = useRef();
   const [state, dispatch] = useReducer(reducer, getMask, init);
 
   const imask = useIMask(state);
   const { value = "", setValue } = imask;
 
-  const inputDivRef = useRef();
-  const customDateInputRef = useRef();
+  const inputBoxClasses = ["discount-input-box"];
+  if (className) {
+    inputBoxClasses.push(className);
+  }
 
-  useEffect(() => {
-    maskType && dispatch({ type: maskType });
+  if (theme || type) {
+    inputBoxClasses.push(`discount-input-box_${theme || type}`);
+  }
+  const inputClasses = ["discount-input-box__current"];
 
-    const $input = (customDateInputRef || imask.ref).current;
-    if (theme && theme === "green") {
-      $input.classList.add("discount-input-box__current_dark");
-    }
+  if (theme && theme === "green") {
+    inputClasses.push("discount-input-box__current_dark");
+  }
 
-    const $div = inputDivRef.current;
-    if (className) {
-      $div.classList.add(...className.split(" "));
-    }
+  let inputStyles = {};
+  let inputBoxStyles = style;
+  if (label) {
+    inputStyles = { width: "unset", padding: "2px 16px 3px" };
+    inputBoxStyles = { ...inputBoxStyles, flexDirection: "column" };
+  }
 
-    if (label) {
-      $div.style.flexDirection = "column";
-      $input.style.width = "unset";
-      $input.style.padding = "2px 16px 3px";
-    }
+  if (autoSize) {
+    inputStyles = {
+      ...inputStyles,
+      width: value.length * 10,
+      maxWidth: 200,
+      minWidth: "1rem",
+    };
+  }
 
-    if (theme || type) {
-      $div.classList.add(`discount-input-box_${theme || type}`);
-    }
-
-    if (autoSize) {
-      $input.style.width = value.length * 10;
-      $input.style.maxWidth = 200;
-      $input.style.minWidth = "1rem";
-    }
-  }, []);
-
+  useEffect(() => maskType && dispatch({ type: maskType }), []);
   useEffect(() => setValue(defaultValue), [defaultValue]);
 
+  const onChangeHandler = (e) => {
+    const value = e.target.value;
+    setValue(value);
+
+    if (typeof onChange === "function") onChange(value);
+  };
+
   return (
-    <div ref={inputDivRef} style={style} className="discount-input-box">
-      {Icon && <img src={Icon} className="discount-input-box__icon" alt="Input Icon" />}
+    <div className={inputBoxClasses.join(" ")} style={inputBoxStyles}>
+      {Icon && (
+        <img src={Icon} className="discount-input-box__icon" alt="Input Icon" />
+      )}
 
       {label && <div className="discount-input-box__label">{label}</div>}
 
       <input
         ref={customDateInputRef}
         onBlur={onBlur}
-        className="discount-input-box__current"
+        className={inputClasses.join(" ")}
         value={value}
-        onChange={(e) => {
-          const value = e.target.value;
-          setValue(value);
-
-          if (typeof onChange === "function") onChange(value);
-        }}
+        style={inputStyles}
+        onChange={onChangeHandler}
         onClick={onClick}
       />
     </div>
