e988826e07da1074a7840777a5cae0d655069a8a
nikitafloy <nikita5051@yandex.ru>
2022-08-11 00:54:28 +0300

    Updates

--- src/__mocks__/index.js
+++
@@ -1,5 +0,0 @@
-import { dates } from "./dates";
-import { promos } from "./promos";
-import { Modal } from "./Modal";
-
-export default { dates, promos, Modal };
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import { Input } from "./Input";
-import { Select } from "./Select";
-import { Textarea } from "./Textarea";
-import { Button } from "./Button";
-import { Modal } from "./Modal";
-import { Board } from "./Board";
-import { App } from "./App";
-
-export { Input, Select, Textarea, Button, Modal, Board, App };
